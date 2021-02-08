0fd397e090efa222f33b4aeaa7634bc2fd721746
Emitsu <emitsu.studios@gmail.com>
2021-02-08 15:51:15 -0300

    Añadido formulario

--- build/js/bundle.min.js
+++ build/js/bundle.min.js
@@ -1,4 +1,13 @@
+//variables globales
+    
 let pagina = 1;
+
+const cita = {
+    nombre: '',
+    fecha: '',
+    hora: '',
+    servicios: []
+}
 
 document.addEventListener('DOMContentLoaded', function() {
     iniciarApp();
@@ -15,8 +24,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
     //Comprueba la pagina actual para ocultar paginacion
     botonesPaginador();
+
+    //Muestra el resumen de la cita o mensaje de error
+    mostrarResumen();
 })
 
+//Funcion que muestra la seccion en la que te encontras
 function mostrarSeccion(){
    
     // ocultar la seccion anterior 
@@ -39,6 +52,7 @@ function mostrarSeccion(){
     
 }
 
+//Funcion que oculta la seccion anterior y muestra la actual
 function cambiarSeccion() {
     const enlaces = document.querySelectorAll('.tabs button');
     enlaces.forEach(enlace => {
@@ -55,6 +69,7 @@ function cambiarSeccion() {
     })
 }
 
+//Funcion que coloca los botones correspondientes de la paginacion
 function botonesPaginador(){
     const paginaSiguiente = document.querySelector('#siguiente');
     const paginaAnterior = document.querySelector('#anterior');
@@ -70,6 +85,7 @@ function botonesPaginador(){
     mostrarSeccion();
 }
 
+//Funcion que avanza la pagina
 function paginaSiguiente() {
     const paginaSiguiente = document.querySelector('#siguiente');
     paginaSiguiente.addEventListener('click', e => {
@@ -79,6 +95,7 @@ function paginaSiguiente() {
 
 }
 
+//Funcion que va pagina atras
 function paginaAnterior() {
     const paginaAnterior = document.querySelector('#anterior');
     paginaAnterior.addEventListener('click', e => {
@@ -88,7 +105,27 @@ function paginaAnterior() {
     });
 }
 
+//validacion
 
+function mostrarResumen() {
+    //object destructuring
+    const { nombre, fecha, hora, servicios } = cita;
+
+    //selector resumen
+    const resumenDiv = document.querySelector('.contenido-resumen'); 
+
+    // validacion del objeto
+    if(Object.values(cita).includes('')) {
+        const faltanDatos = document.createElement('P');
+        faltanDatos.textContent = 'Falta completar algunos campos.';
+        faltanDatos.classList.add('invalidar-cita');
+           
+        //Inyectar en el div
+        resumenDiv.appendChild(faltanDatos); 
+    }
+
+
+}
 
 
 
